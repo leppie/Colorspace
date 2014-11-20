3c770f5af6ce8a77df8e90134c1a74053d6c0418
Llewellyn Pritchard <xacc.ide@gmail.com>
2014-11-20 17:08:43 +0200

    Add missing files

---
+++ Colorspace/ChromaticAdaptation.cs
@@ -0,0 +1,34 @@
+﻿namespace Colorspace
+{
+  static class ChromaticAdaptation
+  {
+    static readonly Matrix3x3 Bradford = new double[,] 
+    {
+      { 0.8951,  0.2664, -0.1614},
+      {-0.7502,  1.7135,  0.0367},
+      { 0.0389, -0.0685,  1.0296}
+    };
+
+    public static XYZ Scale(this XYZ c, XYZ srcwp, XYZ dstwp)
+    {
+      http://www.brucelindbloom.com/Eqn_ChromAdapt.html
+
+      var MA = Bradford;
+      var MA_1 = MA.Invert();
+
+      var d = (MA * dstwp).data;
+      var s = (MA * srcwp).data;
+
+      var PYB = new double[,] 
+      {
+        {d[0]/s[0], 0, 0},
+        {0, d[1]/s[1], 0},
+        {0, 0, d[2]/s[2]}
+      };
+
+      var M = MA_1 * PYB * MA;
+
+      return c * M;
+    }
+  }
+}
---
+++ Colorspace/Coordinates.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Colorspace
+{
+  public class Coordinates
+  {
+    Coordinates() { }
+
+    public xyY R { get; private set; }
+    public xyY G { get; private set; }
+    public xyY B { get; private set; }
+    public xyY Whitepoint { get; private set; }
+
+    public static readonly Coordinates sRGB = new Coordinates
+    {
+      R = new xyY { x = 0.6400, y = 0.3300, Y = 0.2126 },
+      G = new xyY { x = 0.3000, y = 0.6000, Y = 0.7153 },
+      B = new xyY { x = 0.1500, y = 0.0600, Y = 0.0721 },
+      Whitepoint = xyY.D65_WhitePoint
+    };
+  }
+}
